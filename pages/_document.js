6ab9b9a69565413c79ac4b3224d8cf511c266ff8
Liliia Chuiko <chuiko1112@gmail.com>
2022-02-10 16:37:15 +0200

    pwa

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class PWADocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.webmanifest" />
+          <link rel="apple-touch-icon" href="/icon-192x192.png" />
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default PWADocument;
